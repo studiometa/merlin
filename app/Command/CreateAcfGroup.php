6427661e1e1aca5e96428c484521a959f7182242
Ludovic Janot <ludovic@studiometa.fr>
2021-10-18 16:13:55 +0200

    Add acf:setup command
    To save the WordPress theme absolute folder path.

--- app/Command/CreateAcfGroup.php
+++ app/Command/CreateAcfGroup.php
@@ -20,15 +20,15 @@ use Symfony\Component\Console\Output\OutputInterface;
  * - question helper
  *
  * 1. Field group name
- * 2. Field type to add
- *   2.1 list from: image, text, wysiwyg, relation, link, page link, gallery, repeater
- *   2.2 Field name
- *   2.3 If repeater handle subfields
- *     2.3.1 Go back to 2 for subfields
- *   2.4 Do you want to add another field y/n (Recursive fn that goes back to 2)
- * 3. Field group location
- *   3.1 (One step ? Multiple Step ? Might need to add a link to explain what you need to fill ?)
- *   3.1 WordPress content type / equality / Matching rule
+ * 2. Field group location
+ *   2.1 (One step ? Multiple Step ? Might need to add a link to explain what you need to fill ?)
+ *   2.1 WordPress content type / equality / Matching rule
+ * 3. Field type to add
+ *   3.1 list from: image, text, wysiwyg, relation, link, page link, gallery, repeater
+ *   3.3 Field name
+ *   3.3 If repeater handle subfields
+ *     3.3.1 Go back to 3 for subfields
+ *   3.4 Do you want to add another field y/n (Recursive fn that goes back to 2)
  * 4. Recap to view what you are creating
  * 5. Create the field group y/n
  * 6. Create a file filled with all the data in the prompt
@@ -51,9 +51,9 @@ class CreateAcfGroup extends Command
      *
      * @var array
      */
-    protected $data = array(
-        'fields' => array(),
-    );
+    protected $data = [
+        'fields' => [],
+    ];
 
     /**
      * SymfonyStyle
@@ -64,11 +64,11 @@ class CreateAcfGroup extends Command
 
 
     /**
-     * The prompt data, use to create the field group file class.
-     *
+     * The type of fields available to add in a field group.
+     * @see https://www.advancedcustomfields.com/resources/#field-types
      * @var array
      */
-    protected $field_types = array(
+    protected $field_types = [
         'text',
         'wysiwyg',
         'image',
@@ -76,7 +76,7 @@ class CreateAcfGroup extends Command
         'link',
         'page link',
         'gallery',
-    );
+    ];
 
     /**
      * Configure
@@ -96,6 +96,8 @@ class CreateAcfGroup extends Command
      */
     protected function interact(InputInterface $input, OutputInterface $output): void
     {
+        // @todo add a verification of the content of config file, else ask user to run acf:setup
+
         $this->io = new SymfonyStyle($input, $output);
 
         $this->io->title('Welcome to the ACF Field Group Generator');
@@ -112,7 +114,7 @@ class CreateAcfGroup extends Command
 
         $this->data['location']['when']  = $this->io->choice(
             'Show this field group if',
-            array(
+            [
                 'post_type',
                 'post_template',
                 'post_status',
@@ -124,11 +126,11 @@ class CreateAcfGroup extends Command
                 'page_parent',
                 'page',
                 'current_user',
-            )
+            ]
         );
         $this->data['location']['equal'] = $this->io->choice(
             $this->data['location']['when'] . ' is equal to/not equal to',
-            array( '==', '!=' )
+            ['==', '!=']
         );
         $this->data['location']['value'] = $this->io->ask('Show this group when ' .
             $this->data['location']['when'] . ' ' . $this->data['location']['equal']);
@@ -145,7 +147,7 @@ class CreateAcfGroup extends Command
             . $this->data['location']['equal'] . ' ' . $this->data['location']['value']);
         $this->io->newLine(1);
         $this->io->text('Fields');
-        $this->io->table(array( 'type', 'slug', 'label', 'required' ), $this->data['fields']);
+        $this->io->table(['type', 'slug', 'label', 'required'], $this->data['fields']);
     }
 
     /**
@@ -156,14 +158,14 @@ class CreateAcfGroup extends Command
         $field_type  = $this->io->choice('Add a field', $this->field_types);
         $field_slug  = $this->io->ask('Enter the slug of the field');
         $field_label = $this->io->ask('Enter the label of the field');
-        $required    = $this->io->choice('Is the field required ', array('no', 'yes'));
+        $required    = $this->io->choice('Is the field required ', ['no', 'yes']);
 
-        $this->data['fields'][] = array(
+        $this->data['fields'][] = [
             'type'     => $field_type,
             'slug'     => $field_slug,
             'label'    => $field_label,
             'required' => $required,
-        );
+        ];
 
         $add_another = $this->io->confirm('Do you want to add another field ? ', true);
 
@@ -173,7 +175,7 @@ class CreateAcfGroup extends Command
     }
 
     /**
-     * Execute function
+     * Execute command function
      *
      * @param InputInterface  $input input.
      * @param OutputInterface $output output.
@@ -185,8 +187,7 @@ class CreateAcfGroup extends Command
         $confirm = $this->io->confirm('Continue with this action ? ', true);
 
         if ($confirm) {
-            // @todo Add creation of the templates here
-            var_dump($this->data);
+            // @todo Add creation of the templates here, use $this->data
             return Command::SUCCESS;
         }
 
